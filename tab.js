9d2d8b883bcdf9188c1c28fd71ca6e9cca9cc6c9
Jiui <jiui0103@naver.com>
2023-07-10 23:49:19 +0900

    Update tab function: shorten the code with event object and dataset

--- tab.js
+++ tab.js
@@ -1,14 +1,24 @@
-// Menu click function
+// Tab click function
 
 // alert($('.tab-button').length);
 
-for(let i=0; i<$('.tab-button').length; i++){
-    $('.tab-button').eq(i).on('click', function(){
-        $('.tab-button').removeClass('orange');
-        $('.tab-button').eq(i).addClass('orange');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
-      })
+// for(let i=0; i<$('.tab-button').length; i++){
+//     $('.tab-button').eq(i).on('click', function(){
+//       openTab(i);
+//     })
+// }
+
+// Tab function with Eventlistner
+$('.list').click(function(e){
+  openTab(e.target.dataset.btn);
+})
+
+
+function openTab(i){
+  $('.tab-button').removeClass('orange');
+  $('.tab-button').eq(i).addClass('orange');
+  $('.tab-content').removeClass('show');
+  $('.tab-content').eq(i).addClass('show');
 }
 
 
