13c25a980fef45196fde96d131168e5d232c39ef
Jiui <jiui0103@naver.com>
2023-07-10 23:03:26 +0900

    Make tab click function

---
+++ tab.js
@@ -0,0 +1,14 @@
+// Menu click function
+
+// alert($('.tab-button').length);
+
+for(let i=0; i<$('.tab-button').length; i++){
+    $('.tab-button').eq(i).on('click', function(){
+        $('.tab-button').removeClass('orange');
+        $('.tab-button').eq(i).addClass('orange');
+        $('.tab-content').removeClass('show');
+        $('.tab-content').eq(i).addClass('show');
+      })
+}
+
+
